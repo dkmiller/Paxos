c16b2a2fce7152bb573c4f67d70e37e22caabffd
Daniel Miller <dm635@cornell.edu>
2016-10-31 19:46:46 -0400

    new_instance almost complete

--- src/server.py
+++ src/server.py
@@ -9,30 +9,61 @@ incoming = {}
 N = None
 pid = None
 port = None
+threads = {}
 
 def new_instance(kind):
-    return (None, None)
+    global incoming, pid
+    subid = None
+    if kind == 'acceptor':
+        subid = 0
+    elif kind == 'commander':
+        if len(incoming) > 0:
+            subid = max(3, max(incoming.keys())) + 1
+        else:
+            subid = 4
+    elif kind == 'leader':
+        subid = 1
+    elif kind == 'replica':
+        subid = 2
+    elif kind == 'scout':
+        subid = 3
+    incoming[subid] = Queue()
+    def receive():
+        m = incoming[subid].get(block=True).split(':',2)
+        sender_port = int(m[0])
+        sender_subid = int(m[1])
+        msg = m[2]
+        LOG.debug('received: ' + msg)
+        return ((sender_port, sender_subid), msg)
+    def send(recipient, msg):
+        recipient_port, recipient_subid = sender_port
+        msg_to_send = str(recipient_port) + ':' + str(recipient_subid) + ':' + msg
+        LOG.debug('send: ' + msg_to_send)
+    return (send, receive)
 
 def main():
     global N, pid, port
-    pid = int(sys.argv[1]) # This server's id, from 0..N-1
-    N = int(sys.argv[2]) # Total number of servers.
-    port = int(sys.argv[3]) # Port number the server should listen on.
+
+    # Read global state.
+    pid = int(sys.argv[1])
+    N = int(sys.argv[2])
+    port = int(sys.argv[3])
 
     LOG.basicConfig(filename=str(pid) + '.log', level=LOG.DEBUG)
     LOG.debug('pid: %d' % pid)
 
-    send, receive = new_instance('replica')
-    leaders = []
-    for i in xrange(N):
-        leader_port = 20000 + i
-        leader_subid = 1
-        leaders.append((leader_port, leader_subid))
+    # Spawn the necessary threads.
 
+    # Create the replica running on this process.
+    leaders = [(20000+i, 1) for i in xrange(N)]
+    send, receive = new_instance('replica')
+
+    # Spawn the replica.
     replica = Replica(leaders, '', send, receive)
     replica.start()
 
-    LOG.debug('Replica()')
+    LOG.debug('main ends')
 
 if __name__ == "__main__":
     main()
+
