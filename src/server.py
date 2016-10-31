afc0d47a2e799aa586fbc62bd6701e69e904b5f9
Daniel Miller <dm635@cornell.edu>
2016-10-31 17:02:36 -0400

    renamed logging to LOG

--- src/server.py
+++ src/server.py
@@ -1,4 +1,4 @@
-import logging
+import logging as LOG
 import os
 import sys
 
@@ -9,11 +9,11 @@ def main():
     N = int(sys.argv[2]) # Total number of servers.
     port = int(sys.argv[3]) # Port number the server should listen on.
 
-    logging.basicConfig(filename=str(pid) + '.log', level=logging.DEBUG)
-    logging.debug('pid: %d' % pid)
+    LOG.basicConfig(filename=str(pid) + '.log', level=LOG.DEBUG)
+    LOG.debug('pid: %d' % pid)
     
     replica = Replica(pid)
-    logging.debug('Replica()')
+    LOG.debug('Replica()')
 
 if __name__ == "__main__":
     main()
