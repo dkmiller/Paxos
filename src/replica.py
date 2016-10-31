8d8086937552748f77e3920c07e6aa16b323d6af
Daniel Miller <dm635@cornell.edu>
2016-10-31 17:01:33 -0400

    removed unnecessary imports

--- src/replica.py
+++ src/replica.py
@@ -1,6 +1,6 @@
-import logging
+import logging as LOG
 
 class Replica:
     def __init__(self, pid):
-        logging.debug('replica %d called' % pid)
+        LOG.debug('replica %d called' % pid)
 
