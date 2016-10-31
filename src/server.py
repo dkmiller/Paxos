7ff24e921f5f40c9a4b30252cc8d63252a85a099
Daniel Miller <dm635@cornell.edu>
2016-10-30 23:28:30 -0400

    basic log file functionality

--- src/server.py
+++ src/server.py
@@ -1,3 +1,12 @@
 import sys
 
-print "server called: " + str(sys.argv)
+def main():
+    id = int(sys.argv[1])
+    n = int(sys.argv[2])
+    port = int(sys.argv[3])
+    filename = 'log' + str(id) + '.txt'
+    with open(filename, 'w+') as f:
+        f.write("> server %d" % id)
+
+if __name__ == "__main__":
+    main()
