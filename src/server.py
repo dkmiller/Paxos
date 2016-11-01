f37c88fbc43456a0276b75bdab98b6ddc020ea2b
Ashutosh Nath Agarwal <ashutosh.nath.agarwal@gmail.com>
2016-11-01 03:21:13 -0400

    something

--- src/server.py
+++ src/server.py
@@ -1,8 +1,30 @@
 import logging as LOG
 import os
 import sys
+from socket import SOCK_STREAM, socket, AF_INET
+from threading import Thread, Lock
 
 from replica import Replica
+
+root_port = 20000
+address = 'localhost'
+
+class MasterHandler(Thread):
+    def __init__(self, address, port):
+        Thread.__init__(self)
+        self.sock = socket(AF_INET, SOCK_STREAM)
+        self.sock,bind((address, port))
+        self.sock.listen(1)
+        slef.conn, self.addr = self.sock.accept()
+
+    def run(self):
+        while True:
+            try:
+                data = self.conn.recv(1024)
+                if data:
+                    data = data.split('\n')
+                    data = data[:-1]
+
 
 def main():
     pid = int(sys.argv[1]) # This server's id, from 0..N-1
