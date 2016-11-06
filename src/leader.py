d3a98d7e4be5899742827d0a4a9503edda530bb2
Ashutosh Nath Agarwal <ashutosh.nath.agarwal@gmail.com>
2016-11-06 14:50:01 -0500

    leader - in progress

--- src/leader.py
+++ src/leader.py
@@ -3,18 +3,36 @@ from threading import Thread, Lock
 
 class Leader(Thread):
     def __init__(self, acceptors, replicas, send, receive):
+        Thread.__init__(self)
         self.acceptors = acceptors
-        self.active = False
-        self.ballot_num = 0
-        self.proposals = []
         self.replicas = replicas
-
         self.send = send
         self.receive = receive
-        LOG.debug('Leader() called')
+        LOG.debug('LEADER inited')
 
     def run(self):
-        LOG.debug('thread.run() called')
+        LOG.debug('LEADER running')
+        ballot_num = 0
+        active = False
+        proposals = []
+    
+        # TODO
+        spawn scout
+
         while True:
             sender, msg = self.receive()
-            LOG.debug('msg = ' + msg)
+            LOG.debug("LEADER: receive: " + str(msg) + " , SENDER: " + str(sender))
+            msg = msg.split(":")
+            sp = ast.literal_eval(msg[1])
+            s = int(sp[0])
+            p = int(sp[1])
+            
+            # Case 1
+            if msg[0] == "propose":
+                proposals = list(set([sp]).union(proposals))
+                if active:
+                    # TODO
+                    spawn Commander
+
+
+                
